3d917b62ad9d095e7a37c72cb1c8b49f74a5efee
isplicity-jodell <jodell@isplicity.com>
2024-02-21 20:09:36 -0700

    combining files

--- downloader.sh
+++ downloader.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-wget -O /var/www/html/blacklists/ds-block.txt https://www.dshield.org/block.txt
-wget -O /var/www/html/blacklists/sh-drop.txt https://www.spamhaus.org/drop/drop.txt
-wget -O /var/www/html/blacklists/sh-edrop.txt https://www.spamhaus.org/drop/edrop.txt
-wget -O /var/www/html/blacklists/sslipblacklist.txt https://sslbl.abuse.ch/blacklist/sslipblacklist.txt
+#FULLPATH=/var/www/html/blacklists
+FULLPATH=/Users/jamesodell/Documents/git/downloads/
+wget -O $FULLPATH/ds-block.txt https://www.dshield.org/block.txt
+wget -O $FULLPATH/sh-drop.txt https://www.spamhaus.org/drop/drop.txt
+wget -O $FULLPATH/sh-edrop.txt https://www.spamhaus.org/drop/edrop.txt
+wget -O $FULLPATH/sslipblacklist.txt https://sslbl.abuse.ch/blacklist/sslipblacklist.txt
+sed -n 's/\(.*[^0-9]\|\)\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0]\+\).*\(24\).*/\2\/\3/p' $FULLPATH/ds-block.txt > $FULLPATH/ds-block-filtered.txt
+cat ds-block-filtered.txt sh-drop.txt sh-edrop.txt sslipblacklist.txt > combined.txt
+
