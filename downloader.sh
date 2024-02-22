ad4006e68c247daa95e42fb3bed0ce005fcbd77d
isplicity-jodell <jodell@isplicity.com>
2024-02-22 06:56:03 -0700

    additional filters

--- downloader.sh
+++ downloader.sh
@@ -6,5 +6,7 @@ wget -O $FULLPATH/sh-drop.txt https://www.spamhaus.org/drop/drop.txt
 wget -O $FULLPATH/sh-edrop.txt https://www.spamhaus.org/drop/edrop.txt
 wget -O $FULLPATH/sslipblacklist.txt https://sslbl.abuse.ch/blacklist/sslipblacklist.txt
 sed -n 's/\(.*[^0-9]\|\)\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0]\+\).*\(24\).*/\2\/\3/p' $FULLPATH/ds-block.txt > $FULLPATH/ds-block-filtered.txt
-cat $FULLPATH/ds-block-filtered.txt $FULLPATH/sh-drop.txt $FULLPATH/sh-edrop.txt $FULLPATH/sslipblacklist.txt > $FULLPATH/combined.txt
+sed -n  's/\(.*[^0-9]\|\)\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0]\+...\).*/\2/p' $FULLPATH/sh-drop.txt > $FULLPATH/sh-drop-filtered.txt
+sed -n  's/\(.*[^0-9]\|\)\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0]\+...\).*/\2/p' $FULLPATH/sh-edrop.txt > $FULLPATH/sh-edrop-filtered.txt
+cat $FULLPATH/ds-block-filtered.txt $FULLPATH/sh-drop-filtered.txt $FULLPATH/sh-edrop-filtered.txt $FULLPATH/sslipblacklist.txt > $FULLPATH/combined.txt
 
