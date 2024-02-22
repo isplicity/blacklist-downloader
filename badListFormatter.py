8a83f952b751a6a75a212ad44001f04d9a81fbb4
cjoseyIsplicity <cjosey@isplicity.com>
2024-02-22 17:53:32 -0600

    Python 10 Version

--- badListFormatter.py
+++ badListFormatter.py
@@ -5,15 +5,32 @@ with open('variables.json', 'r') as file:
     variables = json.loads(file.read())
 
 
-dsBlock = "https://www.dshield.org/block.txt"
-shDrop = "https://www.spamhaus.org/drop/drop.txt"
-shEDrop = "https://www.spamhaus.org/drop/edrop.txt"
-sslipBL = "https://sslbl.abuse.ch/blacklist/sslipblacklist.txt"
+dsBlockUrl = "https://www.dshield.org/block.txt"
+shDropUrl = "https://www.spamhaus.org/drop/drop.txt"
+shEDropUrl = "https://www.spamhaus.org/drop/edrop.txt"
+sslipBLUrl = "https://sslbl.abuse.ch/blacklist/sslipblacklist.txt"
 
-dsBlock = [f"{row.decode('utf-8').split('\t')[0]}/{row.decode('utf-8').split('\t')[2]}" for row in requests.get(dsBlock).content.split(b"\n") if len(row.decode('utf-8')) > 0 and row.decode('utf-8')[0] != "#"]
-shDrop = [f"{row.decode('utf-8').split(' ;')[0]}" for row in requests.get(shDrop).content.split(b"\n") if len(row.decode('utf-8')) > 0 and row.decode('utf-8')[0] != ";"]
-shEDrop = [f"{row.decode('utf-8').split(' ;')[0]}" for row in requests.get(shEDrop).content.split(b"\n") if len(row.decode('utf-8')) > 0 and row.decode('utf-8')[0] != ";"]
-sslipBL = [f"{row.decode('utf-8')}" for row in requests.get(sslipBL).content.split(b"\n") if len(row.decode('utf-8')) > 0 and row.decode('utf-8')[0] != "#"]
+
+dsBlock = []
+for row in requests.get(dsBlockUrl).content.split(b"\n"):
+    if len(row.decode('utf-8')) > 0 and row.decode('utf-8')[0] != "#":
+        rowList = row.decode('utf-8').split('\t')
+        dsBlock.append(f"{rowList[0]}/{rowList[2]}")
+
+shDrop = []
+for row in requests.get(shDropUrl).content.split(b"\n"):
+    if len(row.decode('utf-8')) > 0 and row.decode('utf-8')[0] != ";":
+        shDrop.append(f"{row.decode('utf-8').split(' ;')[0]}")
+
+shEDrop = []
+for row in requests.get(shEDropUrl).content.split(b"\n"):
+    if len(row.decode('utf-8')) > 0 and row.decode('utf-8')[0] != ";":
+        shDrop.append(f"{row.decode('utf-8').split(' ;')[0]}")
+
+sslipBL = []
+for row in requests.get(sslipBLUrl).content.split(b"\n"):
+    if len(row.decode('utf-8')) > 0 and row.decode('utf-8')[0] != "#":
+        sslipBL.append(f"{row.decode('utf-8')}")
 
 
 def modifyCommand(before, listElement, after):
@@ -44,6 +61,6 @@ with open(f"{variables['default_filepath']}blacklist.rsc", 'w') as file:
     for slip in sslipBL:
         commandBeforeIp = "add list=blacklist timeout=1d address="
         cammandAfterIp = " comment=sslipBL"
-        file.write(f"{modifyCommand(commandBeforeIp, she, cammandAfterIp)}\n")
+        file.write(f"{modifyCommand(commandBeforeIp, slip, cammandAfterIp)}\n")
 
     
